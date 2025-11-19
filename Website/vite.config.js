75ff2bd9a66e1d98ba6c8fb4cc14e6da304d2fb6
Swapnil1001 <sj020420@gmail.com>
2025-11-19 15:16:31 +0530

    Build successful
    Build successful

--- Website/vite.config.js
+++ Website/vite.config.js
@@ -16,6 +16,19 @@ export default defineConfig({
 
   // ⬇⬇ Increase bundle size limit here
   build: {
-    chunkSizeWarningLimit: 5000, // 
-  },
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'EMPTY_BUNDLE') return;
+        warn(warning);
+      },
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                    // chunkSizeWarningLimit: 10000 // 
+                }
+            }
+        }
+  }
 });
